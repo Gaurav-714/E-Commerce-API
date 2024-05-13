5f986dd329cd80ab1eefb29ff562a7bf8d730b93
Gaurav-714 <gauravnagose714@gmail.com>
2024-05-13 12:44:32 +0530

    Filtering Products

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -3,15 +3,29 @@ from rest_framework.views import APIView
 from django.db.models import Q
 from .models import Product
 from .serializers import ProductSerializer
+from .filters import ProductFilter
 
 
 class ProductView(APIView):
     def get(self, request):
-        products = Product.objects.all()
-        search_query = request.GET.get('search')
-        if search_query:
-            products = Product.objects.filter(Q(name__icontains=search_query))
-        serializer = ProductSerializer(products, many=True)
-        return Response({
-            'products': serializer.data
-        })
+        try:
+            products = Product.objects.all()
+            search_query = request.GET.get('search')
+
+            if search_query:
+                products = Product.objects.filter(Q(name__icontains=search_query))
+
+            filterset = ProductFilter(request.GET, queryset=products.order_by('id'))
+            serializer = ProductSerializer(filterset.qs, many=True)
+
+            return Response({
+                'success': True,
+                'data': serializer.data
+            })
+        
+        except Exception as ex:
+            return Response({
+                'success': False,
+                'message': "Something went wrong.",
+                'error': str(ex)
+            })
