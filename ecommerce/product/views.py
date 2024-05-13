0a3ba90594be3c3f5377dcfbae196b3df333b12c
Gaurav-714 <gauravnagose714@gmail.com>
2024-05-13 12:15:08 +0530

    Search for Products

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.db.models import Q
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        search_query = request.GET.get('search')
+        if search_query:
+            products = Product.objects.filter(Q(name__icontains=search_query))
+        serializer = ProductSerializer(products, many=True)
+        return Response({
+            'products': serializer.data
+        })
